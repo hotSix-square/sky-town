4b500a978ce2ea14eec83553ca5e21de6402f6b8
Semy-sudo <songyein620@gmail.com>
2021-12-24 02:04:14 +0900

    news

--- HappyhouseAPI/src/main/java/com/ssafy/happyhouse/util/NewsController.java
+++ HappyhouseAPI/src/main/java/com/ssafy/happyhouse/util/NewsController.java
@@ -2,6 +2,8 @@ package com.ssafy.happyhouse.util;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.util.model.News;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/crawling")
 public class NewsController {
@@ -22,16 +26,22 @@ public class NewsController {
 
 	@GetMapping
 	public ResponseEntity<ArrayList<News>> startCrawl() throws IOException {
+		crawLawData();
+		getNewsInfo();
+		//service 
+		
+		return ResponseEntity.ok().body(newsList);
+	}
+	
+	@ApiOperation(value = "부동산 사이트 뉴스탭을 크롤링 해온다.")
+	private void crawLawData() throws IOException {
 		String address = "https://www.mk.co.kr/news/realestate/";// 부동산 뉴스 url
 		Document rawData = Jsoup.connect(address).timeout(5000).get();
 		blogContent = rawData.select(".desctxt");
 		blogOption = rawData.select(".tit");
-
-		getNewsInfo();
-
-		return ResponseEntity.ok().body(newsList);
 	}
 
+	@ApiOperation(value = "크롤링한 정보를 가공하여 List 에 담는다")
 	private void getNewsInfo() {
 		for (Element option : blogOption) {
 			Elements link = option.select("a");
---
+++ HappyhouseAPI/src/test/java/com/ssafy/happyhouse/util/NewsControllerTest.java
@@ -0,0 +1,15 @@
+package com.ssafy.happyhouse.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewsControllerTest {
+
+	@Test
+	final void testStartCrawl() {
+		
+		//fail("Not yet implemented"); // TODO
+	}
+
+}
