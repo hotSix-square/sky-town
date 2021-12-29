30d157b5964017a75b8a4b946214abcb6fa890ec
Semy-sudo <songyein620@gmail.com>
2021-12-29 22:27:24 +0900

    update

--- HappyhouseAPI/src/test/java/com/ssafy/happyhouse/util/NewsControllerTest.java
+++ HappyhouseAPI/src/test/java/com/ssafy/happyhouse/util/NewsControllerTest.java
@@ -1,12 +1,12 @@
 package com.ssafy.happyhouse.util;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class NewsControllerTest {
 
 	@Test
+	@DisplayName("크롤링 테스트")
 	final void testStartCrawl() {
 		
 		//fail("Not yet implemented"); // TODO
